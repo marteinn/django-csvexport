f4b196a7947f0ca65273d0f011bc9c52b58c0598
Martin Sandström <martin@marteinn.se>
2015-03-17 21:35:58 +0100

    Bumped version

--- csvexport/__init__.py
+++ csvexport/__init__.py
@@ -10,8 +10,8 @@ A django extension that simplifies model to csv conversions.
 
 
 __title__ = "csvexport"
-__version__ = "0.1.0"
-__build__ = 010
+__version__ = "1.0.0"
+__build__ = 100
 __author__ = "Martin Sandström"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015 Martin Sandström"

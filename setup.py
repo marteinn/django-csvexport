7a68b1159b0d2a657b43779393215cc42c7e4608
Martin Sandström <martin@marteinn.se>
2015-08-19 19:48:50 +0200

    Fixed install issue

--- setup.py
+++ setup.py
@@ -3,25 +3,32 @@
 
 import os
 import sys
+import pip
 
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
-from pypandoc import convert
 import csvexport
+
 
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist upload")
     sys.exit()
 
 # Handle requirements
-requires = parse_requirements("requirements/install.txt")
+requires = parse_requirements("requirements/install.txt",
+                              session=pip.download.PipSession())
 install_requires = [str(ir.req) for ir in requires]
 
-requires = parse_requirements("requirements/tests.txt")
+requires = parse_requirements("requirements/tests.txt",
+                              session=pip.download.PipSession())
 tests_require = [str(ir.req) for ir in requires]
 
 # Convert markdown to rst
-long_description = convert('README.md', 'rst')
+try:
+    from pypandoc import convert
+    long_description = convert("README.md", "rst")
+except:
+    long_description = ""
 
 
 setup(
